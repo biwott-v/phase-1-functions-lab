f3d9243f049ec62874eeaf3a61fa079dce35c1fe
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-06 07:43:03 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  blocks -= 42;
+  return Math.abs(blocks);
+}
+function distanceFromHqInFeet(feet) {
+  feet = distanceFromHqInBlocks(feet) * 264;
+  return feet;
+}
+function distanceTravelledInFeet(initialBlock, finalBlock) {
+  let traveled = Math.abs(initialBlock - finalBlock);
+  return traveled * 264;
+}
+function calculatesFarePrice(start, destination) {
+  let traveled = distanceTravelledInFeet(start, destination);
+  if (traveled < 400) {
+    return 0;
+  } else if (traveled > 400 && traveled <= 2000) {
+    return (traveled - 400) * 0.02;
+  } else if (traveled > 2000 && traveled <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+console.log(calculatesFarePrice(50, 55));
